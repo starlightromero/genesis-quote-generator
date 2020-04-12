1a5c348364a55c03a0674985d8b73eee4ff5ce77
Starlight Romero <starlightromero@gmail.com>
2020-04-11 21:42:04 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This project was created with [Brain.js](https://github.com/BrainJS/brain.js).
+
+## Genesis Quote Generator
+
+This project was created using a LSTM neural network.
+Enter a one to three word phrase and a Genesis Breyer P-Orridge style quote will be machine generated.
---
+++ index.js
@@ -0,0 +1,122 @@
+const genesisQuotes = [
+  "People say, 'I'm a woman trapped in a man's body' or 'I'm a man trapped in a woman's body,' but I say 'I'm trapped in a body.'",
+  "Art and life really are the same, and both can only be about a spiritual journey, a path towards a re-union with a supreme creator, with god, with the divine; and this is true no matter how unlikely, how strange, how unorthodox, one's particular life path might appear to one's self or others at any given moment.",
+  "And when in doubt, be extreme.",
+  "After thee accumulation of too much history we have lost our innocence, we cannot easily believe in any explanations. We describe rather than feel, we touch rather than explore, we lust rather than adore.",
+  "Our identity is fictional, written by parents, relatives, education, society.",
+  "the voluntary relinquishing of responsibility for our lives and our actions is one of the greatest enemies of our time.",
+  "All great music is in one way or another psychedelic.",
+  "An exact science is one that admits loss.",
+  "Industrial culture? There has been a phenomena; I don't know whether it's strong enough to be a culture. I do think what we did has had a reverberation right around the world and back.",
+  "I'll never give up being into trying to change the world.",
+  "Thee Temple is a church of only LEADERS, no followers. A radical step. Even thee Nazis, though they bred an elite of leaders, still wanted to control thee masses, lead them and entangle themselves with them. We want thee leaders alone. Fuck thee sleeping masses. We have no desire to be superior rulers of boring, dull masses of people who we despise. We want JUST leaders. A church full of leaders, only leaders and not leading anyone. Merely co- habiting. A separate existence for OUR satisfaction. Why waste all that time, energy and vision dealing with boring masses of people. We've got better things to do. Enjoying and stimulating ourselves. A self-centered religion instead of a crippling, selfless Christian ideal.",
+  "When order is lost, time spits.",
+  "Nothing is forgotten, all is permitted. In a stinking cave, muttering babies scream and scratch, furs undulate in copulation. In one corner, bright-eyed first marks are daubed on a wall. They are marks to function, marks of place, of time. They are marks to draw results and persist beyond one human lifetime. Instinct has arisen, snake-like, coiling itself into intuition and suggesting the very power of suggestion. No one noted down from a book this process, it grew from watching the elements, closeness to life-sources, death-forces that modern persons are divorced from. On this damp stone there is a curve, it is land, horizon, ejaculation, movement.",
+  "The bottom line is that the human species has to realize the human body really is just a cheap suitcase.",
+  "Change comes from reflection.",
+  "Change is not a linear process; it's an all-encompassing process, and it's alive in different ways.",
+  "The biggest way to say, philosophically, you'll never be part of a war is to look completely the opposite of anyone in a war.",
+  "Any artist should stay challenged as long as possible.",
+  "Humans have to realise they're not individuals but individual parts of the same organism, with responsibility to each other.",
+  "All culture, all important culture, is always linked to how people express and experience being alive.",
+  "I refer to myself as 'we.'",
+  "That's really the whole point of art - it's to take something commonplace and draw people on a path so that, all of a sudden, they have a new impression of everything around them.",
+  "The punk rockers said, 'Learn three chords and form a band.' And we thought, 'Why learn any chords?' We wanted to make music like Ford made cars on the industrial belt. Industrial music for industrial people.",
+  "The great irony was that the punks were more conservative and narrow-minded and musically bigoted that anyone else.",
+  "What's incredible with Trent Reznor is how he took all the alienation and the rejection of traditional rock and found a way to encapsulate it in a form that made the public finally get industrial music.",
+  "The status quo is presented as something to aspire to, whereas for us, the status quo was something we wanted to shatter in order to create the space for people to choose for themselves.",
+  "one of the gorgeous things is that we will go from something amazingly serious and important and significant in terms of the world and life, and then do something ludicrous and absurd.",
+  "we have been committed to the idea of evolution on some level and change on some level - that human behaviour may not be changeable, but one has to try and be optimistic and work towards content that might signify change.",
+  "We were already bemoaning the fact that people were using certain accessorised ideas and images that they connected with us - sort of strange buildings and neo-fascist regimes and the 'dark side' of human culture.",
+  "Girls together getting dressed up can be really good fun.",
+  "The human body is not the person. Identity is the way the brain operates; it's memories, it's sensory input and output. The mind is the person.",
+  "The biggest way to say, philosophically, you'll never be part of a war is to look completely the opposite of anyone in a war.",
+  "As a little boy, I never felt comfortable with being human.",
+  "If we confound and break up the proposed unfolding the world impresses upon us, we can give ourselves the space to consider what we want to be as a species.",
+  "Once you believe things are permanent, you're trapped in a world without doors.",
+  "To sell yourself is somewhat debasing, and everyone is selling something.",
+  "Writing is a recording that you can cut up and reassemble. Sound is something you can cut up and reassemble. Film, video - you know, the main tools of culture - can all be cut up and reassembled.",
+  "Everything we do is art, philosophy, mysticism, cultural commentary.",
+  "Everything in our world tends to be built on either ors, and either ors inevitably make enemies.",
+  "Human beings are not capable of creating a thought that truly conceives of this existence. Nobody knows if we are really here, alive, or anything. It's a mystery.",
+  "It has always been my belief that creation, the making of 'art' in any medium or combination of mediums, is a holy act.",
+  "My artwork is in order to seduce people into thinking.",
+  "To be an 'artist' is as much a calling from and to a divine service as becoming a physician, nurse, priest, shaman, or healer.",
+  "Every few years, you have to change your strategy. You have to look at how the world is mutating, and mutate. Not in the same way but in parallel.",
+  "We must embrace unity, not separation - sharing, go back to small, caring communities. Unity, not separation, is what has to happen.",
+  "The good thing about people who are corporate is that they're stupid. So they can be touching something that's precious or radical or special, and they miss the point completely.",
+  "We live in this miraculous technological environment, and yet our human behaviour is still governed by basic impulses from prehistoric times.",
+  "The body is simply the suitcase that carries us around.",
+  "I really feel that I've been unjustly exorcised from the story of psychedelic music.",
+  "I've always felt that all the music I've made is psychedelic.",
+  "There is no distinction between reverence for existence and our senses and/or apathy.",
+  "Pleasure is a cultural weapon. Use it wisely.",
+  "All the great artists illustrate their approach to life in the work they make.",
+  "In the art world, sentimentality and intimacy and the emotive side of lives are considered very uncool. There's nervousness around intimacy.",
+  "Life and art are inseparable.",
+  "People have become obsessed with the greed of celebrity and self-branding and wanting to be known and recognized and succeed in some way, and they're not prepared to share and help each other.",
+  "I always felt that everything that happened was incredibly exhilarating and massively puzzling at the same time. I can even remember, when I was six or seven, digging a hole beneath a tree. And I would go into this tomb, this cave that I had made, and would lie there, meditating, for hours.",
+  "Once you're looking for wisdom, you have to look at why things happen and why people behave how they do: you cannot, in all conscience, accept any form of prejudice.",
+  "Things don't happen in a vacuum, and artists don't make work in a vacuum.",
+  "Even if the world outside is destroying itself and fragmented and paranoid and fearful, the job of the artist is to embrace and hold people and say, 'It's OK, be safe here.'",
+  "There's always a way to say something that could seem really commonplace and make it special again.",
+  "Humanity is a virus.",
+  "My mind jumbles things, reassembles them, and plays with words without even being asked.",
+  "Everyone knows that when things are out of balance, things go wrong.",
+  "I believe in being completely open to the most unlikely explanation.",
+  "I've discovered the joys of happiness.",
+  "A band can be so much more than just a way to play songs.",
+  "The gender is irrelevant; the identity is the one you should try and create for yourself by yourself, and the narrative of your own life becomes your own book.",
+  "We should always be looking for the unity in things instead of the differences.",
+  "I guess I'm dedicated to breaking every inherited mould I can in my private life.",
+  "You have an absolute right to translate poetry in any form with any sound. It's all up for grabs.",
+  "Everyone is telling the truth all of the time... well, it's just that times change.",
+  "When in doubt, make no sense. No sense is good. And nonsense is good.",
+  "I am so sick and tired of being told what I'm supposed to look like!",
+  "Imagination should always be treasured, even when it's slightly off-key.",
+  "Curiosity is a great weapon for the artist.",
+  "We all fall into biological and mental habits. It's an easy way for us to navigate day-to-day work and life, but it also doesn't do us any favours in terms of growing into wisdom, growing into a greater understanding of each other, growing into a deeper relationship - all the things that we really crave.",
+  "I think we can only ultimately change the world by example and by fearlessly embracing what could happen.",
+  "Somebody once asked me, 'What do you do?' and I flippantly answered 'I'm a cultural engineer.' With hindsight, I kind of am - but if I got too self-conscious about it, it wouldn't work.",
+  "I've always aimed to create something pure."
+]
+
+const trainingData = genesisQuotes.map(quote => quote.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/['"]+/g, '')).filter(quote => quote.length < 66)
+
+const net = new brain.recurrent.LSTM();
+
+const quoteSubmitted = () => {
+  document.getElementById('textButton').classList.add('shrink')
+  setTimeout(function() {
+    document.getElementById('textButton').classList.add('invisible')
+    document.getElementById('imgButton').classList.remove('invisible')
+    document.getElementById('imgButton').classList.add('rotate')
+  }, 160)
+}
+
+const quoteLoading = () => {
+  net.train(trainingData, {
+      iterations: 1000,
+      errorThresh: 0.015,
+  })
+}
+
+const quoteLoaded = () => {
+  let userData = document.getElementById('formInput').value
+  let outputData = net.run(userData)
+  document.getElementById('instructions').classList.add('invisible')
+  document.getElementById('showQuote').classList.remove('invisible')
+  document.getElementById('showQuote').innerHTML = `${userData} ${outputData}`
+  document.getElementById('textButton').classList.remove('shrink')
+  document.getElementById('textButton').classList.remove('invisible')
+  document.getElementById('imgButton').classList.add('invisible')
+  document.getElementById('imgButton').classList.remove('rotate')
+}
+
+handleReceiveQuote = () => {
+  quoteSubmitted()
+  setTimeout(function() {
+    quoteLoading()
+    quoteLoaded()
+  }, 300)
+}
